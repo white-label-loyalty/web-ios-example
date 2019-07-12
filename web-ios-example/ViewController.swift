a8c7d49b7694ffde9cbd9708c9bb023af4febf3c
Matthew Webb <matthewowebb@gmail.com>
2019-07-12 16:48:43 +0530

    Initial Commit

---
+++ web-ios-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  web-ios-example
+//
+//  Created by Matthew Webb on 12/07/2019.
+//  Copyright © 2019 White Label Loyalty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
